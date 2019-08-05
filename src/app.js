a775f1131042e7ea73284c5fafa7f26bdfe15422
Samuel Ievins <sievins8012@gmail.com>
2019-08-05 11:24:37 +0100

    Init Create-React-App

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App

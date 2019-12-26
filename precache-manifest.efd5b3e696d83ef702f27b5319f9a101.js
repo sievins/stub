8049a56ede307953b6a39202eba57209cb33869e
Samuel Ievins <sievins8012@gmail.com>
2019-12-26 12:59:24 +0000

    Updates

---
+++ precache-manifest.efd5b3e696d83ef702f27b5319f9a101.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b94e7f3ae80a1a7261e92f3d70ff698",
+    "url": "/stub/index.html"
+  },
+  {
+    "revision": "4f12feef05aa79dd3911",
+    "url": "/stub/static/css/main.d78ee502.chunk.css"
+  },
+  {
+    "revision": "d89e5400d3a19fe81ef3",
+    "url": "/stub/static/js/2.42fcf18f.chunk.js"
+  },
+  {
+    "revision": "4f12feef05aa79dd3911",
+    "url": "/stub/static/js/main.13a70e33.chunk.js"
+  },
+  {
+    "revision": "7182eabb4b946035d808",
+    "url": "/stub/static/js/runtime~main.cbda3759.js"
+  }
+]);
